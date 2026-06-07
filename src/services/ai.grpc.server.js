23393ca5a1ce5128905f6d0c30f6c01265a8d510
long113112113 <nguyenthibe1979@gmail.com>
2026-06-07 08:48:37 +0700

    u

--- src/services/ai.grpc.server.js
+++ src/services/ai.grpc.server.js
@@ -24,7 +24,7 @@ const startServer = () => {
     }
 
     const protoRoot = protoLoader.loadSync(PROTO_PATH, {
-        keepCase: false,
+        keepCase: true,
         longs: String,
         enums: String,
         defaults: true,
