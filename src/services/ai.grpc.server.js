f24682b16b5276d15ecc6a12cdcd3c9b6fdfcef3
long113112113 <nguyenthibe1979@gmail.com>
2026-06-07 08:28:50 +0700

    u

--- src/services/ai.grpc.server.js
+++ src/services/ai.grpc.server.js
@@ -33,7 +33,13 @@ const startServer = () => {
     const pkg = grpc.loadPackageDefinition(protoRoot);
     const serviceCtor = pkg.facerecognition.FaceRecognition.service;
 
-    server = new grpc.Server();
+    server = new grpc.Server({
+        'grpc.keepalive_time_ms': 10000,
+        'grpc.keepalive_timeout_ms': 5000,
+        'grpc.keepalive_permit_without_calls': 1,
+        'grpc.http2.min_time_between_pings_ms': 10000,
+        'grpc.http2.min_ping_interval_without_data_ms': 5000,
+    });
 
     server.addService(serviceCtor, {
         Recognize: (call, callback) => {
